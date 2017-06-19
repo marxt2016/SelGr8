423684c267149321fea6a2b8dbcd9250ffa3f57a
Olga M <marxt@yandex.ru>
2017-06-19 10:46:33 +0400

    Initial commit

---
+++ tasks/src/test/java/ex1/FirstTest.java
@@ -0,0 +1,38 @@
+package ex1;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by qwer on 19.06.2017.
+ */
+public class FirstTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+
+    }
+
+    @Test
+    public void firstTest (){
+        driver.get ("http://www.ya.ru");
+        driver.findElement(By.name("text")).sendKeys("qa");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+
+    }
+
+}
